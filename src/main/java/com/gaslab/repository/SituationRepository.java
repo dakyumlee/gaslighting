03ba44b6b1ea33b40129bf8cba30cd4b78d057c5
dakyumlee <oicrcutie@gmail.com>
2025-07-25 00:13:19 +0900

    java fix

--- src/main/java/com/gaslab/repository/SituationRepository.java
+++ src/main/java/com/gaslab/repository/SituationRepository.java
@@ -1,11 +1,7 @@
 package com.gaslab.repository;
 
-import com.gaslab.model.Statement;
 import com.gaslab.model.Situation;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
 
-public interface StatementRepository extends JpaRepository<Statement, Long> {
-    List<Statement> findBySituation(Situation situation);
+public interface SituationRepository extends JpaRepository<Situation, Long> {
 }
-
--- src/main/java/com/gaslab/repository/StatementRepository.java
+++ src/main/java/com/gaslab/repository/StatementRepository.java
@@ -3,7 +3,6 @@ package com.gaslab.repository;
 import com.gaslab.model.Statement;
 import com.gaslab.model.Situation;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
 
 public interface StatementRepository extends JpaRepository<Statement, Long> {
